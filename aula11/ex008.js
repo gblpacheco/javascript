20cb2201849df17002a8fc5a69a4e2561b8dbcbf
Gabriel Pacheco <gblpacheco@gmail.com>
2024-12-21 09:53:05 -0300

    exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5;
+
+console.log(`A velocidade é ${vel} Km/h`);
+
+if (vel <= 60) {
+  console.log(`Sua velocidade é permitida`);
+} else {
+  console.log(`Sua Velocidade é acima da permitida`);
+}
