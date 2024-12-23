ada9b42fb9f62ec9d3afcac5e0aea9ab9962f68b
Gabriel Pacheco <gblpacheco@gmail.com>
2024-12-23 20:02:44 -0300

    Condições
    Exercícios com condições

---
+++ aula12/ex11.js
@@ -0,0 +1,16 @@
+var idade = 65;
+console.log(`Você tem ${idade} anos.`);
+
+if (idade < 16) {
+  console.log("Não vota");
+} else {
+    if (idade >= 16 && idade < 18) {
+        console.log("Voto facultativo");
+    } else{
+        if (idade >= 18 && idade < 65) {
+            console.log("Voto obrigatório");
+        } else {
+            console.log("Voto facultativo");
+        }
+    }
+}
