bdbc2fb1b9cc41385e088baba659da0696bb1c00
Gabriel Pacheco <gblpacheco@gmail.com>
2024-12-23 21:36:02 -0300

    Exercícios
    Resolução de exercícios

--- aula12ex/ex01/js/script.js
+++ aula12ex/ex01/js/script.js
@@ -1,4 +1,19 @@
-var hora = new Date().getHours();
-var sau = document.getElementById('saudacao');
+function carregar (){
+    var msg = document.getElementById('saudacao');
+    var img = document.getElementById('foto');
+    var data = new Date();
+    var minutos = data.getMinutes();
+    var hora = data.getHours();
+    msg.innerHTML = `${hora} horas e ${minutos} minutos.`;
 
-sau.innerText = hora;
+    if (hora >= 0 && hora < 12) {
+        img.src = 'img/dia.png';
+        document.body.style.background = '#e2cd9f';
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'img/tarde.png';
+        document.body.style.background = '#b9846f';
+    } else {
+        img.src = 'img/noite.jpeg';
+        document.body.style.background = '#515154';
+    }
+}
