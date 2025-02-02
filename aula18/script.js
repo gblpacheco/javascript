19126c41a3172d428528d829a07b8f363b9a2fa2
Gabriel Pacheco <gblpacheco@gmail.com>
2025-02-02 19:42:21 -0300

    Create script.js

---
+++ aula18/script.js
@@ -0,0 +1,12 @@
+// let isMember = false;
+
+// let shipping = isMember ? 2 : 10;
+
+// console.log(isMember ? `Você é membro` : `Você não é membro`)
+// console.log(`Frete: R$ ${shipping}`);
+
+let age = 18;
+
+let isAdult = (age >= 18 ? `Maior de idade` : `Menor de idade`);
+
+console.log(isAdult);
